fe61532cf90185d00376bc546e21e69a8a068a82
Mattias-b-Pettersson <mattep3@gmail.com>
2022-09-07 19:21:09 +0000

    Added CSS

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        })
+    }
+
+})
