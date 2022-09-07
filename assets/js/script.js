0478ed3ce4fb84355bef2e31d5e205a2f763598a
Mattias-b-Pettersson <mattep3@gmail.com>
2022-09-07 20:13:32 +0000

    Added runGame function and also the display functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,97 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                checkAnswer();
+                runGame();
             } else {
-                let gameType = this.getAttribute("data-type");
-                runGame(gameType);
+                let gameInput = this.getAttribute("data-type");
+                updatePlayerSelection;
             }
         })
     }
 
 })
+
+function runGame(gameInput) {
+    let num = math.floor(Math.random() * 4) + 1;
+    
+    if (num === 1)  {
+        displayRock("computer");
+    } else if (num === 2) {
+        displayPaper("computer"); 
+    } else if (num === 3) {
+        displayScissor("computer");
+    } else if (num === 4) {
+        displayLizard("computer");
+    } else if (num === 5) {
+       displaySpock("computer");
+    }
+
+}
+
+
+
+
+function displayRock(input) {
+
+    let computerSide = document.getElementById("computer-selected");
+    let playerSide = document.getElementById("computer-selected");
+    
+    if (input === "computer") {
+            computerSide.innerHTML = `<i class="fa-solid fa-hand-back-fist">`
+    } else {
+        playerSide.innerHTML = `<i class="fa-solid fa-hand-back-fist">`
+    }
+
+}
+
+function displayPaper(input) {
+
+    let computerSide = document.getElementById("computer-selected");
+    let playerSide = document.getElementById("computer-selected");
+    
+    if (input === "computer") {
+            computerSide.innerHTML = `<i class="fa-solid fa-hand"></i>`
+    } else if (input === "player") {
+        playerSide.innerHTML = `<i class="fa-solid fa-hand"></i>`
+    }
+
+}
+
+function displayScissor(input) {
+
+    let computerSide = document.getElementById("computer-selected");
+    let playerSide = document.getElementById("computer-selected");
+    
+    if (input === "computer") {
+            computerSide.innerHTML = `<i class="fa-solid fa-hand-scissors">`
+    } else if (input === "player") {
+        playerSide.innerHTML = `<i class="fa-solid fa-hand-scissors">`
+    }
+
+}
+
+function displayLizard(input) {
+
+    let computerSide = document.getElementById("computer-selected");
+    let playerSide = document.getElementById("computer-selected");
+    
+    if (input === "computer") {
+            computerSide.innerHTML = `<i class="fa-solid fa-hand-lizard"></i>`
+    } else if (input === "player"){
+        playerSide.innerHTML = `<i class="fa-solid fa-hand-lizard"></i>`
+    }
+
+}
+
+function displaySpock(input) {
+
+    let computerSide = document.getElementById("computer-selected");
+    let playerSide = document.getElementById("computer-selected");
+    
+    if (input === "computer") {
+            computerSide.innerHTML = `<i class="fa-solid fa-hand-spock"></i>`
+    } else if (input === "player"){
+        playerSide.innerHTML = `<i class="fa-solid fa-hand-spock"></i>`
+    }
+
+}
