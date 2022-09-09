0e6b7b2617f9d82e0bf40b0500ba2ebb4182c0af
Mattias-b-Pettersson <mattep3@gmail.com>
2022-09-09 19:26:11 +0000

    Added media queries and handleSubmitClicked function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
-
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit") {
-                updateComputerSelection()
-                evaluate();
-            } else {
-                updatePlayerSelection(this);
-            }
-        })
+/** Handles submit button click */
+function handleSubmitClicked() {
+    let playerChoosen = document.getElementById("player-selected").children[0].getAttribute("data-type");
+    if (playerChoosen === "not-chosen" ) {
+        alert("You haven't choosen a hand!");
+    } else {
+    updateComputerSelection();
+    evaluate();
     }
-
-})
+}
 
 /** Updates the players choise when choosing a button */
 function updatePlayerSelection(elem) {
@@ -20,6 +15,7 @@ function updatePlayerSelection(elem) {
 
 }
 
+/** Choses and updates a play for the computer */
 function updateComputerSelection() {
     let num = Math.floor(Math.random() * 4);
     
@@ -28,12 +24,10 @@ function updateComputerSelection() {
 
 /** Looks if it is a win or loss between the player and the computer */
 function evaluate() {
-    let computerChoosen = document.getElementById("computer-selected").children[0].getAttribute("data-type")
-    let playerChoosen = document.getElementById("player-selected").children[0].getAttribute("data-type")
+    let computerChoosen = document.getElementById("computer-selected").children[0].getAttribute("data-type");
+    let playerChoosen = document.getElementById("player-selected").children[0].getAttribute("data-type");
 
-    if (playerChoosen === "notchosen" ) {
-        alert("You haven't choosen a hand!")
-    } else if (playerChoosen === "rock" && (computerChoosen === "lizard" || computerChoosen === "scissor")) {
+    if (playerChoosen === "rock" && (computerChoosen === "lizard" || computerChoosen === "scissor")) {
         updateScore("wins", "You win!");
     } else if (playerChoosen === "paper" && (computerChoosen === "spock" || computerChoosen === "rock")) {
         updateScore("wins", "You win!");
@@ -48,8 +42,23 @@ function evaluate() {
     }
 }
 
-/** this function is used if the player lose, and displays "You lose!" and adds 1 score to the loss counter */
+/** this function is used to update the score and displays "You lose!" or "You win!" */
 function updateScore(outcome, outcomeMessage) {
     document.getElementById("win-loss").innerText = outcomeMessage;
     document.getElementById(outcome).innerText = parseInt(document.getElementById(outcome).innerText) + 1;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                handleSubmitClicked()
+            } else {
+                updatePlayerSelection(this);
+            }
+        });
+    }
+});
+
